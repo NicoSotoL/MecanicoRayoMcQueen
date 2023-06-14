c74e43b1c70c55e91fcf0757182e796dd7a4e435
NicoSotoL <nico.sotol@duocuc.cl>
2023-06-13 20:53:26 -0400

    uwu

--- tallerMecanico/urls.py
+++ tallerMecanico/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('index.html/', views.index, name='index_html'),
-    path('Nosotros.html/', views.nosotros, name='nosotros_html'),
+    path('index', views.index, name='index'),
+    path('nosotros', views.nosotros, name='nosotros'),
 ]
