4f88135bf0faa8e8253e46f56236389bb0a39c0b
NicoSotoL <127602463+NicoSotoL@users.noreply.github.com>
2023-06-17 10:30:36 -0400

    Update urls.py

--- mecanico/urls.py
+++ mecanico/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .tallerMecanico import views
 
 urlpatterns = [
+    path('',views.index, name='root')
     path('admin/', admin.site.urls),
     path('tallerMecanico/',include('tallerMecanico.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
